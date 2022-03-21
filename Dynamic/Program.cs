3542713ef482e98e2c67345752341cea1f50b02a
Michał Kordos <michal.kordos8@gmail.com>
2022-03-21 13:11:20 +0100

    Dynamic

---
+++ Dynamic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic tojestDynamic = "aa";
+            tojestDynamic.ToUpper(); // nie ma żadnych podpowiedzi | ale nie ma też błędu
+
+
+            int mint = 1 + 3;
+            var mvar = 1 + 3;
+            dynamic dyn = 1 + 3;
+            object obj = 1 + 3;
+
+            Console.WriteLine(mint.GetType());
+            Console.WriteLine(mvar.GetType());
+            Console.WriteLine(dyn.GetType());
+            Console.WriteLine(obj.GetType());
+
+
+            dynamic dyna;
+            int a = 20;
+
+            dyna = a;
+
+            string b = "To jest dynamic !!";
+
+            dyna = b;
+
+            DateTime dt = DateTime.Now;
+
+            dyna = dt;
+
+        }
+    }
+}
