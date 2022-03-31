f7f5f9f12cac6b9b60f5c4393a19f2fe5b4803f9
Michał Kordos <michal.kordos8@gmail.com>
2022-03-31 11:15:02 +0200

    Metody rozszerzające

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class Extensions // jesli chcemy stworzyć metodę rozszerzającą nasza klasa musi być statyczna
+    {
+        //zwyczajna metoda którą musimy wywoływać Extensions.IsDateDetween()
+        public static bool IsDateBetween(DateTime date, DateTime from, DateTime to)
+        {
+            return date > from && date < to;
+        }
+
+        public static bool IsBetween(this DateTime date, DateTime from, DateTime to)
+        {//           musimy dopisać this jeśli chcemy stworzyć metodę rozszerzającą
+            return date > from && date < to;
+        }
+
+        public static double Square(this double x)
+        {
+            return x * x;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            DateTime before = now.Subtract(new TimeSpan(7, 0, 0, 0));
+            DateTime after = now.AddDays(2);
+            //ExtensionMethods to metody rozszerzające już istniejące metody - czyli możemy np dodać jakąś własną metodę do stringa czy DateTime
+            //Extensions.IsDateBetween()
+
+            bool isBetween = now.IsBetween(before, after);//używamy metody jako rozszenie
+
+            var number = 4.5;
+
+            var n2 = number.Square();// no to jest fajne
+            
+        }
+    }
+}
